63c3a273340b7edf96d4a6a7a09e856bb044cda1
ldevillard <logandvllrd@gmail.com>
2021-09-01 11:21:15 +0200

    continue researches

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 - https://medium.com/@vgasparyan1995/what-is-an-allocator-c8df15a93ed
 - https://www.it-swarm-fr.com/fr/c%2B%2B/quel-est-lavantage-dutiliser-std-allocator-au-lieu-de-nouveau-en-c/1054557016/
 - https://stackoverflow.com/questions/826569/compelling-examples-of-custom-c-allocators
+> Iterator
+- https://stackoverflow.com/questions/12092448/code-for-a-basic-random-access-iterator-based-on-pointers/31886483
+- https://newbedev.com/how-to-correctly-implement-custom-iterators-and-const-iterators
 > Vector
 - https://www.geeksforgeeks.org/how-to-implement-our-own-vector-class-in-c/
 - https://docs.microsoft.com/fr-fr/cpp/standard-library/vector-class?view=msvc-160&viewFallbackFrom=vs-2019
--- includes/Vector.hpp
+++ includes/Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: ldevilla <ldevilla@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 12:32:04 by ldevilla          #+#    #+#             */
-/*   Updated: 2021/07/14 13:35:12 by ldevilla         ###   ########lyon.fr   */
+/*   Updated: 2021/09/01 11:16:12 by ldevilla         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,23 @@
 
 #include <iostream>
 #include <memory>
+#include <iterator>
 
 namespace ft
 {
 	template <class T, class Alloc = std::allocator<T> >
 	class Vector
 	{
-		public:
-			Vector()
-			{
-				std::cout << "hello i'm vector" << std::endl;
-			}
+		//typedef
+		typedef T value_type; //first arg
+		typedef Alloc allocator_type; //second arg
+		
+		typedef typename allocator_type::reference reference; //reference to an element of our vector
+		typedef typename allocator_type::const_reference const_reference; //const ref **
+		typedef typename allocator_type::pointer pointer; //pointeur **
+		typedef typename allocator_type::const_pointer const_pointer; //const ptr **
+		
+		typedef implementation-defined iterator; //have to implement my own iterator system
 	};
 }
 
